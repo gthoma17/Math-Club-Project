b5dec22e089344285a67b93b71a08cc8ba87e1f6
Greg Thomas <gthoma17@emich.edu>
2014-10-09 16:57:20 -0400

    init commit

---
+++ MathClubDataGatherer/MathClubDataGatherer/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for MathClubDataGatherer project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'MathClubDataGatherer'
+
+SPIDER_MODULES = ['MathClubDataGatherer.spiders']
+NEWSPIDER_MODULE = 'MathClubDataGatherer.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'MathClubDataGatherer (+http://www.yourdomain.com)'
