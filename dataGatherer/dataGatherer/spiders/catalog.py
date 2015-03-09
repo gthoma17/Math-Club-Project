57296f197b97e6075dd43d5b28036886ff965723
Greg Thomas <gthoma17@emich.edu>
2015-03-09 10:19:00 -0400

    Got catalog spider where I want it. Started work on find best minor utility

--- dataGatherer/dataGatherer/spiders/catalog.py
+++ dataGatherer/dataGatherer/spiders/catalog.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 import re
+import pickle
 import scrapy
 from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
@@ -16,10 +17,23 @@ class CatalogSpider(CrawlSpider):
     start_urls = ['http://catalog.emich.edu/content.php?catoid=18&navoid=3608']
 
     rules = (
-        Rule(LinkExtractor(allow=r'preview_program\.php\?catoid=18&poid=\d{4}&returnto=3608'), callback='parse_item', follow=True),
+        Rule(
+            LinkExtractor(allow=r'preview_program\.php\?catoid=18&poid=\d{4}&returnto=3608', deny=r'preview_program\.php\?catoid=18&poid=\d{4}&print=&returnto=3608'), callback='parse_item', follow=True,
+            ),
     )
     def parse_item(self, response):
         item = CatalogItem()
+
+        #extract name
+        try:
+            name = response.xpath('//h1/text()').extract()[0].encode('ascii','replace')
+            if "Minor" not in name:
+                return
+        except IndexError:
+            return
+            name = "UNKNOWN NAME"
+
+        #if this isn't a minor, we don't care~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         #get sections
         unimportant_sections = ["<a name=\"DepartmentInformation\"", "<a name=\"AdvisorInformation\"", "<a name=\"Note\"", "<a name=\"CriticalGraduationInformation\""]
@@ -36,23 +50,34 @@ class CatalogSpider(CrawlSpider):
                 print section_head
                 sections[section_head] = field_text
         #find "Total credits" section, and extract that number
+        total_credits = "UNKNOWN CREDITS"
         for sectionName in sections:
             if "Total" in sectionName:
-                total_credits = re.findall(r'\d+', sectionName)[0]
+                try:
+                    total_credits = re.findall(r'\d+', sectionName)[0].encode('ascii','replace')
+                except IndexError:
+                    pass
+            elif "Minor Requirements:"  in sectionName:
+                try:
+                    total_credits = re.findall(r'\d+', sectionName)[0].encode('ascii','replace')
+                except IndexError:
+                    pass
 
-        #extract name
-        name = response.xpath('//h1/text()').extract()
 
         #attempt to extract catalog IDs for associated classes
         class_ids = []
         for section in sections.values():
             ids = re.findall(r'onclick=\"showCourse\(\'18\', \'\d{6}', section)
-            for thisId in ids:
-                class_ids.append(thisId[27:])
+            if ids:
+                for thisId in ids:
+                    thisId = thisId.encode('ascii','replace')
+                    class_ids.append(thisId[27:])
+            else:
+                pass
 
         #prep the item for return
-        item['name'] = response.xpath('//h1/text()').extract()
+        item['name'] = name
         item['total_credits'] = total_credits
-        item['url'] = response.url
-        item['associated_classes'] = class_ids
+        item['url'] = response.url.encode('ascii','replace')
+        item['associated_classes'] = pickle.dumps(class_ids)
         return item
